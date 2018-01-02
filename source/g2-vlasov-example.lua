acb7f5ced548782b13cb21ad80482f6427de73eb
Ammar <a.hakim777@gmail.com>
2018-01-01 21:26:58 -0500

    Updates to Vlasov app documentation. Most parameters described

---
+++ source/g2-vlasov-example.lua
@@ -0,0 +1,43 @@
+-- Gkyl ------------------------------------------------------------------------
+local Vlasov = require "App.VlasovOnCartGrid"
+
+vlasovApp = Vlasov.App {
+   logToFile = true,
+
+   tEnd = 10.0, -- end time
+   nFrame = 2, -- number of output frames
+   lower = {0.0}, -- configuration space lower left
+   upper = {2*math.pi}, -- configuration space upper right
+   cells = {32}, -- configuration space cells
+   basis = "serendipity", -- one of "serendipity" or "maximal-order"
+   polyOrder = 2, -- polynomial order
+   cflFrac = 1.0, -- CFL "fraction". Usually 1.0
+   timeStepper = "rk2", -- one of "rk2" or "rk3"
+
+   -- decomposition for configuration space
+   decompCuts = {1}, -- cuts in each configuration direction
+   useShared = false, -- if to use shared memory
+
+   -- boundary conditions for configuration space
+   periodicDirs = {1}, -- periodic directions
+   bcx = { }, -- boundary conditions in X
+
+   -- electrons
+   elc = Vlasov.Species {
+      charge = -1.0, mass = 1.0,
+      -- velocity space grid
+      lower = {-6.0},
+      upper = {6.0},
+      cells = {32},
+      decompCuts = {1},
+      -- initial conditions
+      init = function (t, xn)
+	 local x, v = xn[1], xn[2]
+	 return 1/math.sqrt(2*math.pi)*math.exp(-v^2/2)*math.cos(x)
+      end,
+      -- evolve species?
+      evolve = true,
+   },
+}
+-- run application
+vlasovApp:run()
