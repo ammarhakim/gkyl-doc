4e599d8ad1ff906b38850fec63f67b28f29158e4
ammarhakim <a.hakim777@gmail.com>
2020-09-17 13:56:30 -0400

    Switching to previous theme

--- source/conf.py
+++ source/conf.py
@@ -91,8 +91,8 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinxdoc'
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinxdoc'
+#html_theme = "sphinx_rtd_theme"
 #html_theme_path = ["../_themes/sphinx_rtd_theme", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
