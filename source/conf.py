e6353fe708fb06fbadafe422e90e040176a7dc35
ammarhakim <a.hakim777@gmail.com>
2020-09-17 15:48:28 -0400

    no video extention

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,6 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.autosectionlabel',
-    'extensions.video',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
