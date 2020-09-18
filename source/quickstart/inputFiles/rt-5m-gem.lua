c352ceab0639fd82becb620a83c9f60a9f1ecec2
Liang Wang <liang.wang.phys@gmail.com>
2020-09-18 15:45:58 -0400

    fluid: cleanup quickstart

--- source/quickstart/inputFiles/rt-5m-gem.lua
+++ source/quickstart/inputFiles/rt-5m-gem.lua
@@ -1,117 +1,130 @@
--- Gkyl ------------------------------------------------------------------------
 local Moments = require("App.PlasmaOnCartGrid").Moments()
 local Euler = require "Eq.Euler"
 
--- physical parameters
-gasGamma = 5./3.
-elcCharge = -1.0
-ionCharge = 1.0
-ionMass = 1.0
-elcMass = ionMass/25.0
-lightSpeed = 1.0
-epsilon0 = 1.0
-mu0 = 1.0
+-- basic constants
+local gasGamma = 5./3.
+local elcCharge = -1.0
+local ionCharge = 1.0
+local elcMass = 1.0
+local lightSpeed = 1.0
+local mu0 = 1.0
 
-n0 = 1.0
-VAe = 0.5
-plasmaBeta = 1.0
-lambdaOverDi0 = 0.5
-TiOverTe = 5.0
-nbOverN0 = 0.2
-pert = 0.1
-Valf = VAe*math.sqrt(elcMass/ionMass)
-B0 = Valf*math.sqrt(n0*ionMass)
-OmegaCi0 = ionCharge*B0/ionMass
-psi0 = pert*B0
+-- problem-specific characteristic parameters
+local massRatio = 25.0
+local n0 = 1.0
+local vAe0 = 0.5
+local plasmaBeta0 = 1.0
+local lambda_Di0 = 0.5
+local Ti0_Te0 = 5.0
+local nb0_N0 = 0.2
+local perturbationLevel = 0.1
 
-OmegaPe0 = math.sqrt(n0 * elcCharge^2 / (epsilon0 * elcMass))
-de0 = lightSpeed / OmegaPe0
-OmegaPi0 = math.sqrt(n0 * ionCharge^2 / (epsilon0 * ionMass))
-di0 = lightSpeed / OmegaPi0
-lambda = lambdaOverDi0 * di0
+-- derived parameters
+local epsilon0 = 1.0 / lightSpeed^2 / mu0
+local ionMass = elcMass * massRatio
+local vAlf0 = vAe0 * math.sqrt(elcMass / ionMass)
+local B0 = vAlf0 * math.sqrt(n0 * ionMass)
+local OmegaCi0 = ionCharge * B0 / ionMass
+local psi0 = perturbationLevel * B0
 
--- domain size
-Lx = 25.6 * di0
-Ly = 12.8 * di0
+local OmegaPe0 = math.sqrt(n0 * elcCharge^2 / (epsilon0 * elcMass))
+local de0 = lightSpeed / OmegaPe0
+local OmegaPi0 = math.sqrt(n0 * ionCharge^2 / (epsilon0 * ionMass))
+local di0 = lightSpeed / OmegaPi0
+local lambda = lambda_Di0 * di0
 
-momentApp = Moments.App {
+-- domain and time
+local Lx, Ly = 25.6 * di0, 12.8 * di0
+local Nx, Ny = 128, 64
+local tEnd = 25.0 / OmegaCi0
+local nFrame = 5
+
+local momentApp = Moments.App {
    logToFile = true,
 
-   tEnd = 25.0/OmegaCi0,
-   nFrame = 1,
+   tEnd = tEnd,
+   nFrame = nFrame,
    lower = {-Lx/2, -Ly/2},
    upper = {Lx/2, Ly/2},
-   cells = {64, 32},
-   timeStepper = "fvDimSplit",
-
-   -- boundary conditions for configuration space
+   cells = {Nx, Ny},
    periodicDirs = {1}, -- periodic directions
+   timeStepper = "fvDimSplit",
 
    -- electrons
    elc = Moments.Species {
       charge = elcCharge, mass = elcMass,
-
       equation = Euler { gasGamma = gasGamma },
       equationInv = Euler { gasGamma = gasGamma, numericalFlux = "lax" },
-      forceInv = false,
-      -- initial conditions
       init = function (t, xn)
-	 local x, y = xn[1], xn[2]
+         local x, y = xn[1], xn[2]
 
-	 local TeFrac = 1.0/(1.0 + TiOverTe)
-	 local sech2 = (1.0/math.cosh(y/lambda))^2
-	 local n = n0*(sech2 + nbOverN0)
-	 local Jz = -(B0/lambda)*sech2
-	 local Ttotal = plasmaBeta*(B0*B0)/2.0/n0
+         local TeFrac = 1.0 / (1.0 + Ti0_Te0)
+         local sech2 = (1.0 / math.cosh(y / lambda))^2
+         local n = n0 * (sech2 + nb0_N0)
+         local Jz = -(B0 / lambda) * sech2
+         local Ttotal = plasmaBeta0 * (B0 * B0) / 2.0 /n0
 
-	 local rhoe = n*elcMass
-	 local ezmom = (elcMass/elcCharge)*Jz*TeFrac
-	 local ere = n*Ttotal*TeFrac/(gasGamma-1) + 0.5*ezmom*ezmom/rhoe
-	 
-	 return rhoe, 0.0, 0.0, ezmom, ere
+         local rho_e = n * elcMass
+         local rhovz_e = (elcMass / elcCharge) * Jz * TeFrac
+         local thermalEnergy_e = n * Ttotal * TeFrac / (gasGamma-1.0)
+         local kineticEnergy_e = 0.5 * rhovz_e * rhovz_e / rho_e
+         local e_e = thermalEnergy_e + kineticEnergy_e
+
+         return rho_e, 0.0, 0.0, rhovz_e, e_e
       end,
-      evolve = true, -- evolve species?
+      evolve = true,
       bcy = { Euler.bcWall, Euler.bcWall },
    },
 
    -- ions
    ion = Moments.Species {
       charge = ionCharge, mass = ionMass,
-
       equation = Euler { gasGamma = gasGamma },
       equationInv = Euler { gasGamma = gasGamma, numericalFlux = "lax" },
-      forceInv = false,
-      -- initial conditions
       init = function (t, xn)
-	 local x, y = xn[1], xn[2]
+         local x, y = xn[1], xn[2]
 
-	 local TiFrac = TiOverTe/(1.0 + TiOverTe)
-	 local sech2 = (1.0/math.cosh(y/lambda))^2
-	 local n = n0*(sech2 + nbOverN0)
-	 local Jz = -(B0/lambda)*sech2
-	 local Ttotal = plasmaBeta*(B0*B0)/2.0/n0
+         local TiFrac = Ti0_Te0 / (1.0 + Ti0_Te0)
+         local sech2 = (1.0 / math.cosh(y / lambda))^2
+         local n = n0 * (sech2 + nb0_N0)
+         local Jz = -(B0 / lambda) * sech2
+         local Ttotal = plasmaBeta0 * (B0 * B0)/ 2.0 / n0
 
-	 local rhoi = n*ionMass
-	 local izmom = (ionMass/ionCharge)*Jz*TiFrac
-	 local eri = n*Ttotal*TiFrac/(gasGamma-1) + 0.5*izmom*izmom/rhoi
-	 
-	 return rhoi, 0.0, 0.0, izmom, eri
+         local rho_i = n*ionMass
+         local rhovz_i = (ionMass / ionCharge) * Jz * TiFrac
+         local thermalEnergy_i = n * Ttotal * TiFrac / (gasGamma - 1)
+         local kineticEnergy_i = 0.5 * rhovz_i * rhovz_i / rho_i
+         local e_i = thermalEnergy_i + kineticEnergy_i
+
+         return rho_i, 0.0, 0.0, rhovz_i, e_i
       end,
-      evolve = true, -- evolve species?
+      evolve = true,
       bcy = { Euler.bcWall, Euler.bcWall },
    },
 
    field = Moments.Field {
-      epsilon0 = 1.0, mu0 = 1.0,
+      epsilon0 = epsilon0, mu0 = mu0,
       init = function (t, xn)
-	 local x, y = xn[1], xn[2]
+         local x, y = xn[1], xn[2]
+         local pi = math.pi
+         local sin = math.sin
+         local cos = math.cos
 
-	 local Bxb = B0 * math.tanh(y / lambda) 
-	 local Bx = Bxb - psi0 *(math.pi / Ly) * math.cos(2 * math.pi * x / Lx) * math.sin(math.pi * y / Ly) 
-	 local By = psi0 * (2 * math.pi / Lx) * math.sin(2 * math.pi * x / Lx) * math.cos(math.pi * y / Ly)
-	 local Bz = 0.0
+         local Ex, Ey, Ez = 0.0, 0.0, 0.0
 
-	 return 0.0, 0.0, 0.0, Bx, By, Bz
+         local Bx = B0 * math.tanh(y / lambda)
+         local By = 0.0
+         local Bz = 0.0
+
+         local dBx = -psi0 *(pi / Ly) * cos(2 * pi * x / Lx) * sin(pi * y / Ly)
+         local dBy = psi0 * (2 * pi / Lx) * sin(2 * pi * x / Lx) * cos(pi * y / Ly)
+         local dBz = 0.0
+
+         Bx = Bx + dBx
+         By = By + dBy
+         Bz = Bz + dBz
+
+         return Ex, Ey, Ez, Bx, By, Bz
       end,
       evolve = true, -- evolve field?
       bcy = { Moments.Field.bcReflect, Moments.Field.bcReflect },
@@ -120,8 +133,8 @@ momentApp = Moments.App {
    emSource = Moments.CollisionlessEmSource {
       species = {"elc", "ion"},
       timeStepper = "direct",
-   },   
+   },
 
 }
--- run application
+
 momentApp:run()
