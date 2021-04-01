f348e5669f3070623ae27caeb869064a117895d1
ammarhakim <a.hakim777@gmail.com>
2021-03-31 20:39:26 -0400

    Better figure

---
+++ source/dev/ssp-rk-abs-stability-plot.py
@@ -0,0 +1,26 @@
+from pylab import *
+style.use('postgkyl.mplstyle')
+
+fmt = {}
+
+X = linspace(-5.5, 1, 100)
+Y = linspace(-3, 3, 100)
+XX, YY = meshgrid(X,Y)
+ZZ = -XX + 1j*YY
+WW2 = abs(1-ZZ+0.5*ZZ**2)
+cs = contour(XX, YY, WW2, levels=[1], colors='r')
+
+WW3 = abs(1-ZZ+0.5*ZZ**2-1.0/6.0*ZZ**3)
+cs = contour(XX, YY, WW3, levels=[1], colors='k')
+
+WW3_4 = abs((ZZ**4-8*ZZ**3+24*ZZ**2-48*ZZ+48)/48.0)
+cs = contour(XX, YY, WW3_4, levels=[1], colors='m')
+
+xlabel(r'$\Delta t \lambda$')
+ylabel(r'$\Delta t \omega$')
+title(r'Absolute Stability Region')
+
+grid()
+axis('image')
+savefig('ssp-rk-abs-stability.png', dpi=150)
+show()
